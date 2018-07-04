6508fc7cce07d5b43b190006b264e04c5edc3ddd
Vyacheslav Safarian <vyacheslav.safarian@gmail.com>
2018-07-04 14:15:40 +0300

    added animation hourglass changed app icon

--- app/src/main/java/anoda/mobi/anoda_turn_timer/ui/splash/SplashPresenter.kt
+++ app/src/main/java/anoda/mobi/anoda_turn_timer/ui/splash/SplashPresenter.kt
@@ -1,5 +1,7 @@
 package anoda.mobi.anoda_turn_timer.ui.splash
 
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import kotlinx.coroutines.experimental.Job
@@ -12,19 +14,27 @@ import java.util.concurrent.TimeUnit
 class SplashPresenter : MvpPresenter<SplashView>() {
 
     companion object {
-        val START_DELAY: Long = 2
+        private const val START_DELAY: Long = 1300
+        private const val DURATION_ANIMATE: Long = 1000
     }
 
     private lateinit var startJob: Job
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
+        startAnimation()
         startTimeAndStartNextActivity()
+    }
+
+    private fun startAnimation() {
+        val rotate = RotateAnimation(180f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        rotate.duration = DURATION_ANIMATE
+        viewState.startAnimation(rotate)
     }
 
     private fun startTimeAndStartNextActivity() {
         startJob = launch(UI) {
-            delay(START_DELAY, TimeUnit.SECONDS)
+            delay(START_DELAY, TimeUnit.MILLISECONDS)
             viewState.startTimerActivity()
         }
     }
--- app/src/main/java/anoda/mobi/anoda_turn_timer/ui/splash/SplashView.kt
+++ app/src/main/java/anoda/mobi/anoda_turn_timer/ui/splash/SplashView.kt
@@ -1,8 +1,10 @@
 package anoda.mobi.anoda_turn_timer.ui.splash
 
+import android.view.animation.RotateAnimation
 import com.arellomobile.mvp.MvpView
 
 interface SplashView : MvpView {
 
     fun startTimerActivity()
+    fun startAnimation(animator: RotateAnimation)
 }
